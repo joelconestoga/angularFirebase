4ad07abf64b0616cc000865557fd602fb807af08
Joel Matsu <matsu.joel@gmail.com>
2018-02-18 14:10:43 -0500

    Added and configured angularfire2

--- angular-firebase/src/app/app.module.ts
+++ angular-firebase/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
 
+// FIREBASE Config
+export const firebaseConfig = {
+  apiKey: "xxx",
+  authDomain: "xxx",
+  databaseURL: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
