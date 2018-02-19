e518107c488450576f9a1ea0f91278aac13fea68
Joel Matsu <matsu.joel@gmail.com>
2018-02-18 22:03:34 -0500

    Scrolling the messages to the last one automatically

--- angular-firebase/src/app/home-page/home-page.component.ts
+++ angular-firebase/src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { AngularFireList } from 'angularfire2/database';
 import { AF } from '../providers/af';
 import { Observable } from 'rxjs/Observable';
@@ -8,20 +8,32 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewChecked {
 
   public newMessage: string;
   public messages: Observable<any>;
-  
+
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+
   constructor(public afService: AF) {
     this.messages = this.afService.messages;
   }
 
   ngOnInit() {}
 
-  sendMessage() {
-    console.log('new message: ', this.newMessage);
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch(err) { console.log('Scroll to bottom failed yo!') }
+  }
+  
+  sendMessage(){
     this.afService.sendMessage(this.newMessage);
+    this.newMessage = '';
   }
 
   isYou(email) {
